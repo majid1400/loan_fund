8943b34d0bd24d13560bd36ae3754fc496363287
majid behnamfard <m.b1400v5@gmail.com>
2022-03-13 20:14:15 +0330

    add test_member_duplicate

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import AccountCreateForm
+from .models import Members
+
+
+class DashboardTest(TestCase):
+    def setUp(self):
+        self.member = Members.objects.create(
+            group_id=1,
+            name="ali",
+            family="alavi"
+        )
+
+    def test_member_duplicate(self):
+        form_data = {'group_id': 2, 'name': 'ali', 'family': 'alavi'}
+        form = AccountCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue(form.errors)
