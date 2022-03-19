f9b721a7232c75901214cae63bfebca2e2a1c9c2
majid behnamfard <m.b1400v5@gmail.com>
2022-03-20 01:10:51 +0330

    add unittest test_create_update_models_setting

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from .forms import AccountCreateForm
-from .models import Members, Transaction
+from .models import Members, Transaction, Setting
 from .views import qs_trans_merge_members_transaction_create_view
 
 
@@ -53,7 +53,20 @@ class DashboardTest(TestCase):
         Transaction.objects.create(
             Fund='30000', loan_p='4000000', payer_name='ali', members=member
         )
-        list = qs_trans_merge_members_transaction_create_view()
-        self.assertEqual(list[0]['member'], member)
-        self.assertEqual(list[0]['trans']['Fund'], '30000')
-        self.assertEqual(list[0]['trans']['loan_p'], '4000000')
+        result = qs_trans_merge_members_transaction_create_view()
+        self.assertEqual(result[0]['member'], member)
+        self.assertEqual(result[0]['trans']['Fund'], '30000')
+        self.assertEqual(result[0]['trans']['loan_p'], '4000000')
+
+    def test_create_update_models_setting(self):
+        data = {'loan_ratio': '20', 'number_months_loan_repayment': '200', 'minimum_share': '1000'}
+        self.client.post(reverse("setting"), data=data)
+
+        data2 = {'loan_ratio': '2', 'number_months_loan_repayment': '20', 'minimum_share': '200000'}
+        self.client.post(reverse("setting"), data=data2)
+
+        setting = Setting.objects.all()
+        self.assertEqual(len(setting), 1)
+        self.assertEqual(setting[0].loan_ratio, 2)
+        self.assertEqual(setting[0].number_months_loan_repayment, 20)
+        self.assertEqual(setting[0].minimum_share, 200000)
